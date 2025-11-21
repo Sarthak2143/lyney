3ad27480015a13684e7dd203fc681c25ca5b810a
Sarthak Tomar <sarthaktomar2143@gmail.com>
2025-11-21 16:00:47 +0530

    use roles using types.Content which stores user's prompt

--- main.py
+++ main.py
@@ -3,22 +3,29 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
   if len(sys.argv) != 2:
     print(f"Usage: {sys.argv[0]} prompt")
     sys.exit(1)
-  prompt = sys.argv[1]
+
+  prompt: str = sys.argv[1]
   load_dotenv()
-  api_key = os.environ.get("GEMINI_API_KEY")
+  api_key: str | None = os.environ.get("GEMINI_API_KEY")
   client = genai.Client(api_key=api_key)
+
+  messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+  ]
   response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents=prompt
+    model="gemini-2.0-flash-001",
+    contents=messages,
   )
   print(response.text)
-  print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-  print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+  print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")  # pyright: ignore[reportOptionalMemberAccess]
+  print(f"Response tokens: {response.usage_metadata.candidates_token_count}")  # pyright: ignore[reportOptionalMemberAccess]
 
 
 if __name__ == "__main__":
