feaf10c6accfee20d7eb9424350809030054acde
Sarthak Tomar <sarthaktomar2143@gmail.com>
2025-11-22 13:03:50 +0530

    add function to get file info

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+
+def get_files_info(working_dir, dir=".") -> str:
+  try:
+    abs_wrk_dir: str = os.path.abspath(working_dir)
+    full_path: str = os.path.abspath(os.path.join(working_dir, dir))
+    if not os.path.isdir(full_path):
+      return f'Error: "{dir}" is not a directory'
+    # check if dir exists under working_dir
+    if os.path.commonpath([abs_wrk_dir, full_path]) != abs_wrk_dir:
+      return (
+        f'Error: Cannot list "{dir}" as it is outside the permitted working directory'
+      )
+    file_stats: list[str] = []
+    for file in os.listdir(full_path):
+      file_path = os.path.join(full_path, file)
+      # building string to make debugging ez for llm
+      file_stats.append(
+        f"- {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={str(os.path.isdir(file_path))}"
+      )
+    return "\n".join(file_stats)
+  except Exception as e:
+    return f"Error: {e}"
