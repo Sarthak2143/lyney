3aae3b47e4187832638b576a2ea4232bf24aee6f
Sarthak Tomar <sarthaktomar2143@gmail.com>
2025-11-26 01:55:33 +0530

    add feedback loop to continously iterate on results.

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 MAX_CHARS_TO_READ: int = 10000
+MAX_TRIES = 20
 WORKING_DIR = "./calculator"  # TODO: hardcoding for now, will change later
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from config import MAX_TRIES
 from prompts import system_prompt
 from schemas import available_functions, call_function
 
@@ -30,14 +31,25 @@ def main() -> None:
   if verbose:
     print(f"User prompt: {prompt}")
 
-  messages = [
+  messages: list[types.Content] = [
     types.Content(role="user", parts=[types.Part(text=prompt)]),
   ]
 
-  generate_content(client, messages, verbose)
+  for i in range(MAX_TRIES):
+    # basic loop to test agentic workflow, too tired rn :/
+    # todo: create a better chat ui, more like opencode or gemini code
+    try:
+      res = generate_content(client, messages, verbose)
+      if res:
+        print(f"Final response: {res}")
+        break
+    except Exception as e:
+      print(f"Error: {e}")
 
 
-def generate_content(client, messages, verbose):
+
+
+def generate_content(client, messages, verbose) -> str | None:
   response = client.models.generate_content(
     model="gemini-2.0-flash-001",
     contents=messages,
@@ -45,6 +57,11 @@ def generate_content(client, messages, verbose):
       tools=[available_functions], system_instruction=system_prompt
     ),
   )
+
+  if response.candidates:
+    for candidate in response.candidates:
+      messages.append(candidate.content)
+
   if verbose:
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")  # pyright: ignore[reportOptionalMemberAccess]
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")  # pyright: ignore[reportOptionalMemberAccess]
@@ -55,14 +72,16 @@ def generate_content(client, messages, verbose):
   fn_resp: list[types.Part] = []
   for fn_call in response.function_calls:
     result: types.Content = call_function(fn_call, verbose)
-    if not fn_call.parts or not result.parts[0].function_response:  # pyright: ignore[reportOptionalSubscript]
+    if not result.parts or not result.parts[0].function_response:  # pyright: ignore[reportOptionalSubscript]
       raise Exception("empty function call result.")
-    fn_resp.append(result.parts[0])  # pyright: ignore[reportOptionalSubscript]
+    fn_resp.append(result.parts[0])  # pyright: ignore[reportOptionalSubscript, reportArgumentType]
     if verbose:
       print(f"-> {result.parts[0].function_response.response}")  # pyright: ignore[reportOptionalSubscript]
 
-    if not fn_resp:
-      raise Exception("no function responses generated, exiting.")
+  if not fn_resp:
+    raise Exception("no function responses generated, exiting.")
+  for fn in fn_resp:
+    messages.append(types.Content(role="user", parts=[types.Part(function_response=fn.function_response)]))
 
 
 if __name__ == "__main__":
